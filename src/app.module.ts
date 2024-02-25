587293ef3fa094867f937d22b242bc18e79520f1
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2024-02-25 17:44:12 +0300

    cretae posts rescource + entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [DatabaseModule,ConfigModule.forRoot({isGlobal:true})],
+  imports: [DatabaseModule,ConfigModule.forRoot({isGlobal:true}), PostModule],
   controllers: [AppController],
   providers: [AppService],
 })

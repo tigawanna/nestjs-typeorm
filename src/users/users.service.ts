275239db7d6e7994f0a6f015870be48598c877f7
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2024-02-28 21:10:05 +0300

    init auth branch

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { QueryFailedError, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 
